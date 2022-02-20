280c3b0335528abb2cac129e336f23e581d5216b
Tedfulk <98295827+Tedfulk@users.noreply.github.com>
2022-02-20 16:08:52 -0500

    date parking validated

--- script.js
+++ script.js
@@ -8,7 +8,27 @@ let weekDay = ["Mon", "Tue", "Wed", "Thu", "Fri"]
 let startDate = document.getElementById("start-date")
 
 
+// validation for date parking not to be allowed to reserve for days in past
+let today = new Date()
 
+let dd = today.getDate()
+let mm = today.getMonth() + 1
+let yyyy = today.getFullYear()
+if (dd > 31) {
+    showMessage("day must be less than 31")
+}
+if (dd < 0) {
+ showMessage("day must be greater than 0")
+}
+if (mm < 11) {
+    mm = '0' + mm
+}
+
+todayDate = yyyy + '-' + mm + '-' + dd
+// startDate.value = todayDate
+startDate.min = todayDate
+
+// for loop needs to go around the number of days for however many of days they are parking. for loop needs to push onto the array that will then check to see if it is a weekend or weekday and then it will add 5 or 7 to the sum
 let d = Date(startDate.value)
 let day = days[d.getDay()]
 console.log(day)
@@ -17,20 +37,25 @@ function isWeekend(day) {
   return true
  } else {
   return false
- };
-let finalTotal = ""
- for (let today of weekDay) {
- if isWeekend(today) {
-  let finalTotal += "7+"
+}}
+let i = 0
+while (i < day) {
+  i++;
   
- } else {
-  finalTotal += ""
- }
- }
+}
+
+//  for (let today of weekDay) {
+//  if isWeekend(today) {
+//   let finalTotal += "7"
+  
+//  } else {
+//   finalTotal += "5"
+//  }
+//  }
 
 // };
 // let dd = d.getDate()
-// whyyyyyy can I not get day to equal days and the value inside of startDate
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
